72f713cc74b29995850597f79210ec2f4abe055b
Tina Wuest <tina.wuest@rackspace.com>
2014-09-10 14:23:05 -0500

    Bump version

--- lib/cloudflock.rb
+++ lib/cloudflock.rb
@@ -4,9 +4,5 @@ require 'cloudflock/errstr'
 # Public: Encapsulate all functionality related to the CloudFlock API and any
 # apps built with such.
 module CloudFlock
-<<<<<<< HEAD
-  VERSION = '0.7.2'
-=======
-  VERSION = '0.7.3-dev'
->>>>>>> working
+  VERSION = '0.7.3'
 end
